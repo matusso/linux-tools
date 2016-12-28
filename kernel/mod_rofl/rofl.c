1b799103872ed440e200fccf339a8ec861cd286a
Matúš Bursa <burso@burso.eu>
2016-12-28 19:17:36 +0100

    new jokes added

--- kernel/mod_rofl/rofl.c
+++ kernel/mod_rofl/rofl.c
@@ -3,8 +3,8 @@
 #include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/random.h>
-
 #include <asm/uaccess.h>
+
 MODULE_AUTHOR("Matus Bursa <burso@burso.eu>");
 MODULE_DESCRIPTION("rofl - generating jokes to /dev/rofl");
 MODULE_LICENSE("Beerware");
@@ -17,7 +17,15 @@ static ssize_t rofl_read(struct file *, char *, size_t, loff_t *);
 static const char rofl_jokes[][MAX_SIZE] = { 
 	"I dreamt I was forced to eat a giant marshmallow. When I woke up, my pillow was gone.\n",
 	"My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.\n",
-	"It is so cold outside I saw a politician with his hands in his own pockets.\n"
+	"It is so cold outside I saw a politician with his hands in his own pockets.\n",
+	"What's the difference between snowmen and snowladies? Snowballs \n",
+	"Why did the blonde get excited after finishing her puzzle in 6 months? -- The box said 2-4 years!\n",
+	"I am a nobody, nobody is perfect, therefore I am perfect.\n",
+	"My mom never saw the irony in calling me a son-of-a-bitch.\n",
+	"I used to like my neighbors, until they put a password on their Wi-Fi.\n",
+	"Stalking is when two people go for a long romantic walk together but only one of them knows about it.\n",
+	"How do you seduce a fat woman? Piece of cake.\n",
+	"What do you call a blonde with a brain? A golden retriever.\n"
 };
 
 static const struct file_operations rofl_fops = {
